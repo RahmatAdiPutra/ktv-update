b5d8ca555fd34f5a593c9907e77844c2923eebbe
aman <rahmat.adi.putra.21@gmail.com>
2019-07-30 01:31:27 +0700

    simple-app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
 <meta name="msapplication-TileColor" content="#22b9ff">
 <meta name="msapplication-config" content="{{ asset('favicon/browserconfig.xml') }}">
 <meta name="theme-color" content="#22b9ff">
-<base href="/">
+<base href="{{ url('') }}/">
 <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('favicon/apple-touch-icon.png') }}">
 <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon/favicon-32x32.png') }}">
 <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon/favicon-16x16.png') }}">
