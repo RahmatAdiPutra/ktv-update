abadec14a488617b3bbfbf78be280ffedbdb069e
aman <rahmat.adi.putra.21@gmail.com>
2019-10-08 00:54:43 +0700

    simple-app

--- app/Http/Controllers/Web/Tool/ConvertController.php
+++ app/Http/Controllers/Web/Tool/ConvertController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Web\Tool;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Models\Song;
+use App\Models\Tool\Song;
 use App\Models\Tool\SongMap;
 use Illuminate\Support\Facades\File;
 
@@ -13,14 +13,14 @@ class ConvertController extends Controller
     public function index(Request $request)
     {
         /*
-        1. ambil satu baris data file video di tabel song_maps dari database 192.168.70.64
-        2. cek file video di server 192.168.7.224
+        1. ambil satu baris data tabel song_maps di database 192.168.70.64
+        2. cek file di server 192.168.7.224
            - jika ada, lanjut step 3
-           - jika tidak ada, kembali step 1
-        3. cek kode file di tabel song dari database 192.168.7.226
-           - jika ada, data diganti
+           - jika tidak ada, kembali step 1 dan update flag_check=1
+        3. cek kode file di tabel song database 192.168.7.226
+           - jika ada, file_path update
            - jika tidak ada, tambah baru
-        4. rename file video di server 192.168.7.224
+        4. rename file di server 192.168.7.224
         */
 
         // 1
---
+++ app/Models/Tool/Song.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\Tool;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Song extends Model
+{
+    protected $connection = 'dev';
+}
