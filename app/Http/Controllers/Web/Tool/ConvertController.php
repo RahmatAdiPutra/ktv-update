a781a6fb539aec48d7be0aaa80e32f3ff7fbeb4d
aman <rahmat.adi.putra.21@gmail.com>
2019-10-07 23:09:19 +0700

    simple-app

--- app/Http/Controllers/Web/Tool/ConvertController.php
+++ app/Http/Controllers/Web/Tool/ConvertController.php
@@ -4,18 +4,47 @@ namespace App\Http\Controllers\Web\Tool;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\Song;
 use App\Models\Tool\SongMap;
+use Illuminate\Support\Facades\File;
 
 class ConvertController extends Controller
 {
     public function index(Request $request)
     {
-        $start = $request->get('start', 0);
-        $limit = $request->get('length', 25);
-        $query = SongMap::select('*');
-        $paginate = $query->skip($start)
-            ->paginate($limit)
-            ->toArray();
-        return $this->responseSuccess($paginate);
+        /*
+        1. ambil satu baris data file video di tabel song_maps dari database 192.168.70.64
+        2. cek file video di server 192.168.7.224
+           - jika ada, lanjut step 3
+           - jika tidak ada, kembali step 1
+        3. cek kode file di tabel song dari database 192.168.7.226
+           - jika ada, data diganti
+           - jika tidak ada, tambah baru
+        4. rename file video di server 192.168.7.224
+        */
+        $songMap = SongMap::select('*')->first();
+        $pathinfo = pathinfo($songMap->file_name);
+
+        // $search = File::glob('/home/cyber/public_html/new/*.*');
+        // $search = File::glob('/home/cyber/public_html/new/CALL YOU MINE#THE CHAINSMOKERS FT BEBE REXHA#BARAT#LEFT.mp4');
+        return $search = $this->searchFile('/media/hdd2/new/Music', 'CALL YOU MINE#THE CHAINSMOKERS FT BEBE REXHA#BARAT#LEFT.mp4');
+
+        $song = Song::select('*')->where('file_path', 'like', '%'.$pathinfo['filename'].'%')->first();
+
+        $filename = str_slug($songMap->description, '_');
+        dd($search, $filename, $songMap->toArray(), $song->toArray());
+        // return $this->responseSuccess($data);
+    }
+
+    public function searchFile($filepath, $filename) {
+        $manuals = [];
+        $filesInFolder = File::allFiles($filepath);
+
+        foreach($filesInFolder as $path)
+        {
+            $data = pathinfo($path);
+            $manuals[] = $data;
+        }
+        return $manuals;
     }
 }
