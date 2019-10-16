c831983ba7b50306277e29c677e79bf6d06509cb
aman <rahmat.adi.putra.21@gmail.com>
2019-10-17 01:58:11 +0700

    simple-app

--- app/Console/Commands/Convert.php
+++ app/Console/Commands/Convert.php
@@ -3,8 +3,9 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
-use App\Http\Controllers\Web\Tool\ConvertController;
 use Illuminate\Support\Facades\File;
+use App\Models\Tool\Setting;
+use App\Http\Controllers\Web\Tool\ConvertController;
 
 class Convert extends Command
 {
@@ -13,9 +14,18 @@ class Convert extends Command
      *
      * @var string
      */
-    protected $signature = 'convert:mp4';
+    // protected $signature = 'convert:mp4';
     // protected $signature = 'convert:mp4 {test? : Testing}'; // argument
-    // protected $signature = 'convert:mp4 {--T|test= : Testing}'; // option
+    // protected $signature = 'convert:mp4 {--T|test : Testing}'; // option without value
+    // protected $signature = 'convert:mp4 {--T|test= : Testing}'; // option with value
+
+    protected $signature = 'convert:mp4
+                            {--a|all-job : Run all jobs step by step}
+                            {--r|rename : First step run job rename}
+                            {--s|save : Second step run job save data}
+                            {--c|convert : Third step run job convert to mp4 (in the form of a shell script file)}
+                            {--m|make : Create shell script rename file to new name and original name (for backup name file)}
+                            {--e|execute : Run shell script}';
 
     /**
      * The console command description.
@@ -42,28 +52,56 @@ class Convert extends Command
     public function handle()
     {
         try {
-            // $this->output->writeln($this->option('test'));
-            // $this->output->writeln($this->argument('test'));
-            
+            // $setup = Setting::get('dropBox');
             $setup = json_decode(File::get(public_path('dropBox.json')), true);
+
             $convert = new ConvertController();
-            $renameFile = $convert->file($setup);
 
-            // $this->output->writeln('Run job rename file');
-            // $this->rename($renameFile['newname']);
-
-            // $this->output->writeln('Run job save data');
-            // $convert->save($setup, $convert->song($setup));
-
-            // $this->output->writeln('Run job make shell script convert to mp4');
-            // $convertFile = $convert->convert($setup);
-            // File::put($setup[env('DROP_BOX')]['path']['script'].$setup['script']['name']['convert'].'_'.$setup['lang'].$setup['script']['extension'], implode("\n", $convertFile['convert']));
-
-            // $script = $this->choice('Execute script ?', $setup['script']['name'], 'newname');
-            // $this->output->writeln('Run job rename file use <info>'.$script.'<info>');
-            // $this->rename($renameFile[$script]);
-            // $filepath = $setup[env('DROP_BOX')]['path']['script'].$script.'_'.$setup['lang'].$setup['script']['extension'];
-            // shell_exec('sh '.$filepath);
+            $jobs = collect([
+                $this->option('all-job'),
+                $this->option('rename'),
+                $this->option('save'),
+                $this->option('convert'),
+                $this->option('make'),
+                $this->option('execute')
+            ]);
+            
+            if ($jobs->sum() > 1) {
+                $this->output->writeln('Command too many options');
+            } else {
+                if ($this->option('all-job')) {
+                    $this->output->writeln('Run all job step by step');
+                    $this->rename($convert, $setup);
+                    $this->save($convert, $setup);
+                    $this->convert($convert, $setup);
+                } else if ($this->option('rename')) {
+                    $this->output->writeln('Run job rename');
+                    $this->rename($convert, $setup);
+                } else if ($this->option('save')) {
+                    $this->output->writeln('Run job save data');
+                    $this->save($convert, $setup);
+                } else if ($this->option('convert')) {
+                    $this->output->writeln('Run job convert to mp4');
+                    $this->convert($convert, $setup);
+                } else if ($this->option('make')) {
+                    $this->output->writeln('Create shell script rename file to new name and original name (for backup name file)');
+                    $renameFile = $convert->file($setup);
+                    $filepath_original = $setup[env('DROP_BOX')]['path']['script'].$setup['script']['name']['original'].'_'.$setup['lang'].$setup['script']['extension'];
+                    $filepath_newname = $setup[env('DROP_BOX')]['path']['script'].$setup['script']['name']['newname'].'_'.$setup['lang'].$setup['script']['extension'];
+                    $commands_original = implode("\n", $renameFile['original']);
+                    $commands_newname = implode("\n", $renameFile['newname']);
+                    $this->make($filepath_original, $commands_original);
+                    $this->make($filepath_newname, $commands_newname);
+                } else if ($this->option('execute')) {
+                    $this->output->writeln('Run shell script');
+                    $script = $this->choice('Choose script for run ?', $setup['script']['name'], 'newname');
+                    $filepath = $setup[env('DROP_BOX')]['path']['script'].$script.'_'.$setup['lang'].$setup['script']['extension'];
+                    $this->output->writeln('Run <info>'.$filepath.'<info>');
+                    $this->executeScript($filepath);
+                } else {
+                    // 
+                }
+            }
             $this->line('Done....');
         } catch (\Exception $e) {
             $this->error($e->getMessage());
@@ -71,16 +109,37 @@ class Convert extends Command
         }
     }
 
-    public function test()
+    public function rename($convert, $setup)
     {
-        return 'aaaaaa';
-    }
-
-    public function rename($commands)
-    {
+        $renameFile = $convert->file($setup);
+        $commands = $renameFile['newname'];
+        
         foreach($commands as $command)
         {
             shell_exec($command);
         }
+    }
+
+    public function save($convert, $setup)
+    {
+        $convert->save($setup, $convert->song($setup));
+    }
+
+    public function convert($convert, $setup)
+    {
+        $convertFile = $convert->convert($setup);
+        $filepath = $setup[env('DROP_BOX')]['path']['script'].$setup['script']['name']['convert'].'_'.$setup['lang'].$setup['script']['extension'];
+        $commands = implode("\n", $convertFile['convert']);
+        $this->make($filepath, $commands);
+    }
+
+    public function make($filepath, $commands)
+    {
+        File::put($filepath, $commands);
+    }
+
+    public function executeScript($filepath)
+    {
+        shell_exec('sh '.$filepath);
     }
 }
